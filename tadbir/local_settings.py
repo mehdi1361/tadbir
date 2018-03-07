db219a7d26de7d1fd74b54a379bf74a31e8b789a
mehdi1361 <mhd.mosavi@gmail.com>
2018-03-07 11:46:28 +0330

    add postgres databases

--- tadbir/local_settings.py
+++ tadbir/local_settings.py
@@ -4,20 +4,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 # DEBUG = False
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'tadbir_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '13610522',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tadbir_db',
+        'USER': 'postgres',
+        'PASSWORD': '13610522',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
